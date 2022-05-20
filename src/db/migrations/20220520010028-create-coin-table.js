8a0373d07c41c5d5d489fbaa056bf4860d249178
Federico Medel <88804900+FedeMedel@users.noreply.github.com>
2022-05-19 23:08:09 -0300

    Coin Table Migration
    Coin Table Migration

---
+++ src/db/migrations/20220520010028-create-coin-table.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('Coins', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+
+      },
+
+      ticker: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+
+      unitDolarPrice: {
+        type: Sequelize.DataTypes.FLOAT,
+        allowNull: false
+      },
+
+      createdAt: Sequelize.DataTypes.DATE,
+      updatedAt: Sequelize.DataTypes.DATE,
+      deletedAt: Sequelize.DataTypes.DATE,
+
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Coins');
+  }
+  
+};

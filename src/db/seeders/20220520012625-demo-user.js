d48ce74ca298950222549fc92ad92a900fbfe6ff
Santiago Santamaria <santiago.santamaria@gmail.com>
2022-05-26 21:38:11 -0300

    more users for seeder
    more users for seeder

--- src/db/seeders/20220520012625-demo-user.js
+++ src/db/seeders/20220520012625-demo-user.js
@@ -21,6 +21,86 @@ module.exports = {
                 createdAt: new Date,
                 updatedAt: new Date
             },
+            {
+                firstName: 'Juan',
+                lastName: 'Dominguez',
+                email: 'juandgz@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                firstName: 'Analia',
+                lastName: 'Beltrandi',
+                email: 'anabel1987@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                firstName: 'Joshua',
+                lastName: 'Blake',
+                email: 'jbwalker@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                firstName: 'Aron',
+                lastName: 'Mcnamara',
+                email: 'aronmc1992@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                firstName: 'Karen',
+                lastName: 'Silva',
+                email: 'karensilva98@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                firstName: 'Silvia',
+                lastName: 'Correa',
+                email: 'scorrea_21@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                firstName: 'Sebastian',
+                lastName: 'Muller',
+                email: 'smuller@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                firstName: 'Alex',
+                lastName: 'Curello',
+                email: 'alecurr1@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                firstName: 'Solimano',
+                lastName: 'Shakur',
+                email: 'solinmahrshakur@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
+            {
+                firstName: 'Federico',
+                lastName: 'Prince',
+                email: 'fedeprinceitaly@gmail.com',
+                password: '123454',
+                createdAt: new Date,
+                updatedAt: new Date
+            },
         ], {});
 
     },

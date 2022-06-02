d1c6ed07928adb63797d6d22232145e8929d7c5c
Federico Medel <88804900+FedeMedel@users.noreply.github.com>
2022-06-02 20:01:43 -0300

    userModel

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -16,13 +16,41 @@ module.exports = (sequelize, DataTypes) => {
         }
     }
     User.init({
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+        }
     }, {
         sequelize,
         modelName: 'User',
+        tableName: 'users'
     });
     return User;
 };

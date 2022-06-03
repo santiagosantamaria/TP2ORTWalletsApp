2fb2818d23decc0ee744d9e96dac91fdd032159c
AlexTeper99 <ateper@fi.uba.ar>
2022-06-03 00:32:37 -0300

    change seeder notification

--- src/db/seeders/20220603002049-seederNotifications.js
+++ src/db/seeders/20220603002049-seederNotifications.js
@@ -5,13 +5,13 @@ const { randAlphaNumeric } = require('@ngneat/falso')
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 12; i++) {
       await queryInterface.bulkInsert('notifications',[{
         title: "Noticia " + i,
         text: "Este es el texto de la notificacion " + i,
         date: new Date,
         seen: 0,
-        userId: 2,
+        userId: i,
         createdAt: new Date,
         updatedAt: new Date
       }

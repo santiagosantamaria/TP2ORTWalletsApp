3e9c249c763ae48bce6cc9ff07d2b052ad097bc8
AlexTeper99 <ateper@fi.uba.ar>
2022-06-03 00:12:36 -0300

    create /notifications and /getusernotification in index.js

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ constexpress= require('express')
 
 const app =express();
 
+const {Notification, Coin,User} = require('./src/db/models')
+
 //http://localhost:5555/
 
 //rutas
 app.get('/', function (req,res) {
     res.send('hola')
+})
+
+app.get('/notifications', async function (req,res){
+    let notifications = await Notification.findAll()
+    return res.send(notifications)
+})
+
+//get user notifications
+app.get('/getusernotification', async function (req,res){
+    const john = await User.findByPk(2);
+    const notification = await john.getNotifications();
+
+    return res.send(notification)
 })
 
 app.listen(5555);

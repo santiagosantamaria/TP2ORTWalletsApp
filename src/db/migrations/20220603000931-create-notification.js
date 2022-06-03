3dbfc4c133cd93978dd54023796107226592c921
AlexTeper99 <ateper@fi.uba.ar>
2022-06-02 22:11:21 -0300

    create migracion notification

---
+++ src/db/migrations/20220603000931-create-notification.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('notifications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      title: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+      },
+      text: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+      },
+      date: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false
+      },
+      seen: {
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      deletedAt: Sequelize.DataTypes.DATE
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Notifications');
+  }
+};

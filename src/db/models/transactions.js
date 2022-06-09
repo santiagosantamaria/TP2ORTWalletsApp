7d641180aa50dad02e10d14f2efa9b2e8b0cb4a5
AlexTeper99 <ateper@fi.uba.ar>
2022-06-09 03:10:26 -0300

    create transaction model y migration

---
+++ src/db/models/transactions.js
@@ -0,0 +1,47 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transactions extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Transactions.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    walletId: {
+      type: DataTypes.INTEGER,
+      unique: false,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+
+
+  }, {
+    sequelize,
+    modelName: 'Transactions',
+  });
+  return Transactions;
+};

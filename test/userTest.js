d3edcb1203e5b213ec251846bcdbad90dedcab1b
Santiago Santamaria <santiago.santamaria@gmail.com>
2022-06-09 20:03:18 -0300

    test 2

--- test/userTest.js
+++ test/userTest.js
@@ -6,13 +6,11 @@ const { assert } = chai;
 
   
 describe('Users in DB', function () {
-    it('Users Have to be 12', async function () {
+    it('Users Have to be 13', async function () {
         const users = await axios.get('http://localhost:5555/users/all');
-        // console.log(users);
-        assert.equal(users.data.length, 12);
+        assert.equal(users.data.length, 13);
     });
 });
-
 
 describe('User Insertion in DB', function () {
     it('User should be inserted in DB', async function () {
@@ -27,5 +25,80 @@ describe('User Insertion in DB', function () {
             },
         });
         assert.equal(res.status,201);
+    });
+});
+
+
+// describe('Edit User in DB', function () {
+//     it('User info should be edited in DB', async function () {
+        
+//         let login = await axios({
+//             method: 'post',
+//             url: 'http://localhost:5555/users/login',
+//             data: {
+//                 email: "san@gmail.com", 
+//                 password: "1234"
+//             },
+//         }).then(function(login) {
+//             console.log(login);
+//             let res = axios({
+//                 method: 'put',
+//                 url: 'http://localhost:5555/users/update',
+//                 data: {
+//                     firstName: "Santiago", 
+//                     lastName: "STM", 
+//                     email:    "san@gmail.com",
+//                     password: "1234"
+//                     },
+//             });
+//             assert.equal(res.status,201);
+//         });
+        
+//     });
+// });
+
+describe('Edit User in DB', function () {
+    it('User info should be edited in DB', async function () {
+        let userid = 1;
+        let res = await axios({
+                method: 'put',
+                url: 'http://localhost:5555/users/update/'+ userid,
+                data: {
+                    firstName: "Juan", 
+                    lastName: "Administrador", 
+                    email:    "admin@mail.com",
+                    password: "12345"
+                    },
+            });
+            assert.equal(res.status,201);
+        });
+        
+});
+
+
+describe('Buy a coin with USDT', function () {
+    it('User should login and buy a coin', async function () {
+        
+        let login = await axios({
+            method: 'post',
+            url: 'http://localhost:5555/users/login',
+            data: {
+                email: "san@gmail.com", 
+                password: "1234"
+            },
+        }).then(function(login) {
+            console.log(login.data + " login OK ");
+            let res = axios({
+                method: 'put',
+                url: 'http://localhost:5555/coins/buy',
+                data:
+                    {
+                        "tickerSearch": "LTC", 
+                        "quantity": 999
+                    },
+            });
+            assert.equal(res.status,201);
+        });
+        
     });
 });

7dfad24c2f5d9fb23aa6028f929e80e57bf6147c
CurelloAlejoMF <curello.alejo@gmail.com>
2022-06-13 00:49:42 -0300

    Update Tags. Belongto Coin, init fix

--- src/db/models/tag.js
+++ src/db/models/tag.js
@@ -4,20 +4,43 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Tag extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+
+      Tag.belongsTo(models.Coin, {foreignKey: "coinId"})
+
+
     }
   }
   Tag.init({
-    name: DataTypes.STRING
-  }, {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    coinId: {
+      type: DataTypes.INTEGER,
+      unique: false,
+      allowNull: false
+    },
+
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+
+
+
+  },
+
+   {
     sequelize,
     modelName: 'Tag',
+    tableName: 'tags'
   });
   return Tag;
 };

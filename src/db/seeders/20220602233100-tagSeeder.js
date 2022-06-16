12d6cd1db1de710610da15625aa10ace777aed82
Federico Medel <88804900+FedeMedel@users.noreply.github.com>
2022-06-15 21:10:20 -0300

    fix tag seeder

--- src/db/seeders/20220602233100-tagSeeder.js
+++ src/db/seeders/20220602233100-tagSeeder.js
@@ -45,11 +45,8 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+    await queryInterface.bulkDelete('Tags', null, {});
+     
   }
 };
